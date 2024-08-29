9c6a282134dc1fd2cc5957c01d77b93bc9ffa9cf
Aniket Garate <garateaniket11@gmail.com>
2024-08-29 13:52:26 +0530

    Add DB configs and connection

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 
 const connectDB = async ()=>{
-    await mongoose.connect()
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        // console.log(connectionInstance);
+        
+        console.log(`\n Mongo DB Connected !! DB Host: ${connectionInstance.mongoose.connections[0].host }`);
+        
+    } catch (error) {
+        console.log("MONGOBD CONNECTION ERROR :  ", error);
+        process.exit(1)
+    }
+         
 }
 
 
